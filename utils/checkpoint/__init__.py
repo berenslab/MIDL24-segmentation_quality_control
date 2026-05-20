3baa406943ca73d2332988da60be3bea1f04f181
juliusge <julius.gervelmeyer@student.uni-tuebingen.de>
2026-05-20 13:48:37 +0200

    The `bunch` dependency is not maintained anymore. Replaces `trained/FRUNet_*.pth` with bunch-free checkpoints and adds `utils/checkpoint/` helpers for saving/converting weights. Removes now-unused `import bunch` from `0_pass_forward.ipynb`. Further added pyproject for consistent and working python environment.

---
+++ utils/checkpoint/__init__.py
@@ -0,0 +1,8 @@
+from .io import convert_legacy_checkpoint, load_checkpoint, plainize, save_checkpoint
+
+__all__ = [
+    "convert_legacy_checkpoint",
+    "load_checkpoint",
+    "plainize",
+    "save_checkpoint",
+]
