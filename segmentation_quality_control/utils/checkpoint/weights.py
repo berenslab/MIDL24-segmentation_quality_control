fc82ce39bdff473dda38912e2931f41a11a30cd3
juliusge <julius.gervelmeyer@student.uni-tuebingen.de>
2026-05-28 16:25:51 +0200

    change weights target repo -- revert once PR was merged into berenslab main

--- segmentation_quality_control/utils/checkpoint/weights.py
+++ segmentation_quality_control/utils/checkpoint/weights.py
@@ -16,10 +16,10 @@ DOWNLOAD_CHUNK_SIZE = 1024 * 1024
 DOWNLOAD_TIMEOUT_SECONDS = 300
 
 VESSEL_WEIGHTS_GIT_URL = (
-    "https://github.com/berenslab/MIDL24-segmentation_quality_control.git"
+    "https://github.com/juliusge/MIDL24-segmentation_quality_control.git" # TODO
 )
 # Weights location on GitHub; PyPI wheels do not bundle checkpoint files.
-VESSEL_WEIGHTS_GIT_REF = "make-package-without-bunch"
+VESSEL_WEIGHTS_GIT_REF = "weights-download" # TODO
 VESSEL_WEIGHTS_GIT_SUBDIR = "trained"
 VESSEL_WEIGHTS_RAW_BASE = (
     "https://raw.githubusercontent.com/berenslab/MIDL24-segmentation_quality_control"
